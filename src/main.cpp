add40def4a1c71626207efcfe407b850ffc61bff
MiracleStone <shystone95@gmail.com>
2022-08-07 00:33:10 +0800

    Added CompileShader()

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,110 @@
-#include <iostream>
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
 
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cassert>
+
+GLuint CompileShader(GLenum aShaderType, const char* aShaderPath)
+{
+	switch (aShaderType)
+	{
+	case GL_VERTEX_SHADER:
+		[[fallthrough]]
+	case GL_FRAGMENT_SHADER:
+		break;
+	default: assert(false && "Unsupported shader type");
+	}
+
+	std::stringstream shaderFile;
+	shaderFile << std::ifstream(aShaderPath).rdbuf();
+	std::string shaderStr = shaderFile.str();
+	const char* shaderSrc = shaderStr.c_str();
+
+	GLuint shader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(shader, 1, &shaderSrc, nullptr);
+	glCompileShader(shader);
+
+	return shader;
+}
+
+void ProcessInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, true);
+	}
+}
+
 int main() {
-    std::cout << "Hello world" << std::endl;
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Playground", nullptr, nullptr);
+	if (window == nullptr)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	glViewport(0, 0, 800, 600);
+
+	auto frameBufferSizeCallback = [](GLFWwindow* window, int width, int height) {
+		glViewport(0, 0, width, height);
+	};
+
+	glfwSetFramebufferSizeCallback(window, frameBufferSizeCallback);
+
+	// Prepare vertex data
+	float vertices[] = {
+	-0.5f, -0.5f, 0.0f,
+	 0.5f, -0.5f, 0.0f,
+	 0.0f,  0.5f, 0.0f
+	};
+
+	GLuint vertexBuffer;
+	glGenBuffers(1, &vertexBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	GLuint vertexShader = CompileShader(GL_VERTEX_SHADER, "shader/vertex.shader");
+	GLuint fragmentShader = CompileShader(GL_FRAGMENT_SHADER, "shader/fragment.shader");
+
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	glUseProgram(shaderProgram);
+
+	glDeleteProgram(vertexShader);
+	glDeleteProgram(fragmentShader);
+
+
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		ProcessInput(window);
+
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
+	return 0;
 }
